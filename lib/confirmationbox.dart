c18211fc1e4ea26be83760b128291c32ca3d1ffa
Jestin Antony <jestifi@gmail.com>
2022-06-07 22:08:33 +0530

    Confirmation box

--- lib/confirmationbox.dart
+++ lib/confirmationbox.dart
@@ -1,7 +1,39 @@
 library confirmationbox;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+Function? ConfirmationBox({
+  required BuildContext context,
+  Function? confirmFunction,
+  required String title,
+  required String body,
+  bool no = true,
+  String yesText = "Yes",
+  String noText = "No",
+}) {
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: Text(title.toString()),
+      content: Text(body.toString()),
+      actions: <Widget>[
+        no == true
+            ? TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  noText,
+                  style: const TextStyle(color: Colors.black),
+                ),
+              )
+            : Container(),
+        TextButton(
+          onPressed: confirmFunction as void Function()?,
+          child: Text(
+            yesText,
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    ),
+  );
 }
